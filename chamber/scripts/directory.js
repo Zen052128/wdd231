8ce47a5f76e510286503282ebd33e8743ce91024
Zen052128 <kzdepaloma@gmail.com>
2026-07-13 14:26:34 +0800

    later changes for chamber of commerce page

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -17,3 +17,18 @@ currentYear.innerHTML = year;
 const modify = document.querySelector("#lastModified");
 const lastModified = document.lastModified;
 modify.innerHTML = lastModified;
+
+const navLinks = document.querySelectorAll(".nav-link");
+const pageTitle = document.querySelector("#hero");
+
+navLinks.forEach(link => {
+    link.addEventListener("click", function(event) {
+        event.preventDefault();
+
+        pageTitle.textContent = this.textContent;
+
+        document.querySelector(".nav-link.active")?.classList.remove("active");
+
+        this.classlist.add("active")
+    })
+})
