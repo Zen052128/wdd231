b7d9e4e92fddb1867547ea98081ee10f67f17ad0
Zen052128 <kzdepaloma@gmail.com>
2026-07-13 23:44:32 +0800

    changes in js and all

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -29,6 +29,77 @@ navLinks.forEach(link => {
 
         document.querySelector(".nav-link.active")?.classList.remove("active");
 
-        this.classlist.add("active")
-    })
-})
+        this.classList.add("active");
+    });
+});
+
+const activeLinkOnLoad = document.querySelector(".nav-link.active");
+if (activeLinkOnLoad) {
+    pageTitle.textContent = activeLinkOnLoad.textContent;
+};
+
+const cards = document.querySelector("#business-cards");
+
+const getCompanyData = async () => {
+    try {
+        const response = await fetch("data/member.json");
+        const data = await response.json();
+        displayCompanies(data.companies);
+        // console.log(data.companies);
+    } catch (error) {
+        console.error("Error fetching data:", error)
+    };
+};
+
+const displayCompanies = (companies) => {
+    companies.forEach((company) => {
+        let card = document.createElement("section");
+        card.classList.add("companyCard");
+
+        let name = document.createElement("h2");
+        let image = document.createElement("img");
+        let infoDiv = document.createElement("div");
+
+        let email = document.createElement("p");
+        let phone = document.createElement("p");
+        let url = document.createElement("p");
+
+        name.textContent = `${company.companyName}`;
+        image.setAttribute("src", `images/${company.image}`);
+        image.setAttribute("loading", "lazy")
+        image.setAttribute("width", "200")
+        image.setAttribute("height", "200")
+        infoDiv.classList.add("card-info");
+
+        email.textContent = `${company.email}`;
+        phone.textContent = `${company.phone}`;
+        url.textContent = `${company.website}`;
+
+        infoDiv.appendChild(email);
+        infoDiv.appendChild(phone);
+        infoDiv.appendChild(url);
+
+        card.appendChild(name);
+        card.appendChild(image);
+        card.appendChild(infoDiv);
+
+        cards.appendChild(card);
+    });
+};
+
+getCompanyData();
+
+const sourtButtons = document.querySelectorAll(".sort-btn");
+const sortChangeButton = document.querySelector("#business-cards");
+const sortDesignChange = document.querySelector(".companyCard")
+
+sourtButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        
+        document.querySelector(".sort-btn.active")?.classList.remove("active");
+
+        button.classList.add("active");
+        sortChangeButton.classList.toggle("flex");
+        sortDesignChange.classList.toggle("flex");
+    });
+});
