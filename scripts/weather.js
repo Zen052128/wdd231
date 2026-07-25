bc0d5fd27ed28ce0f1f962ec47b0ed9d22010ff7
Zen052128 <kzdepaloma@gmail.com>
2026-07-25 20:25:31 +0800

    new changes

---
+++ scripts/weather.js
@@ -0,0 +1,4 @@
+const currentTemp = document.querySelector("#current-temp");
+const weatherImg = document.querySelector("#weather-icon");
+const figureDesc = document.querySelector("figcaption");
+const url = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={a2c7441e2e7a56c0156ef735151a0666}"
